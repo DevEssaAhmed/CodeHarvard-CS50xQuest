2e5fc1fd644906a6ca8219fd8d77285957317edb
Essa Ahmed <essaahmedsiddiqui@gmail.com>
2023-03-14 19:23:12 +0500

    Name Program

---
+++ Week 1/What's Your Name/name.c
@@ -0,0 +1,9 @@
+# include <cs50.h>
+# include <stdio.h>
+
+int main(void) 
+{
+    string first = get_string("What's your name? ")
+    string second = get_string("What's your name? ")
+    printf("hello,%s %s\n",first,last);
+}
